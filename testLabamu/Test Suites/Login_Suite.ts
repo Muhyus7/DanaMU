30b90d65e5f59ed18fe20cf8cbccd1aefa77f59f
MUHAMMAD YUSUF <102980841+Muhyus7@users.noreply.github.com>
2025-12-20 00:17:30 +0700

    Add files via upload

---
+++ testLabamu/Test Suites/Login_Suite.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login_Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>cbf319ff-9bf3-4d8f-a0ff-93092bb9a373</testSuiteGuid>
+   <testCaseLink>
+      <guid>fcfc9696-4b99-4781-bd69-60481502f3fa</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/Successfull Email Login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
